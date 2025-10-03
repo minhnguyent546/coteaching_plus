718faa6469c5b7f9d53b4172cce718f718c16cda
minhnguyent546 <minhnguyent546@gmail.com>
2025-10-03 21:33:17 +0700

    fix: caught some bugs in `loss_coteaching` function

--- loss.py
+++ loss.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import torch 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
@@ -7,12 +7,12 @@ import numpy as np
 from numpy.testing import assert_array_almost_equal
 
 # Loss functions
-def loss_coteaching(y_1, y_2, t, forget_rate, ind, noise_or_not):
-    loss_1 = F.cross_entropy(y_1, t, reduction='none')
+def loss_coteaching(logits1, logits2, labels, forget_rate, ind, noise_or_not):
+    loss_1 = F.cross_entropy(logits1, labels, reduction='none')
     ind_1_sorted = np.argsort(loss_1.cpu().data).cuda()
     loss_1_sorted = loss_1[ind_1_sorted]
 
-    loss_2 = F.cross_entropy(y_2, t, reduction='none')
+    loss_2 = F.cross_entropy(logits2, labels, reduction='none')
     ind_2_sorted = np.argsort(loss_2.cpu().data).cuda()
     loss_2_sorted = loss_2[ind_2_sorted]
 
@@ -29,10 +29,10 @@ def loss_coteaching(y_1, y_2, t, forget_rate, ind, noise_or_not):
     pure_ratio_1 = np.sum(noise_or_not[ind[ind_1_update]])/float(num_remember)
     pure_ratio_2 = np.sum(noise_or_not[ind[ind_2_update]])/float(num_remember)
 
-    loss_1_update = F.cross_entropy(y_1[ind_2_update], t[ind_2_update])
-    loss_2_update = F.cross_entropy(y_2[ind_1_update], t[ind_1_update])
+    loss_1_update = F.cross_entropy(logits1[ind_2_update], labels[ind_2_update])
+    loss_2_update = F.cross_entropy(logits2[ind_1_update], labels[ind_1_update])
 
-    return torch.sum(loss_1_update)/num_remember, torch.sum(loss_2_update)/num_remember, pure_ratio_1, pure_ratio_2
+    return torch.sum(loss_1_update), torch.sum(loss_2_update), pure_ratio_1, pure_ratio_2
 
 def loss_coteaching_plus(logits, logits2, labels, forget_rate, ind, noise_or_not, step):
     outputs = F.softmax(logits, dim=1)
@@ -45,26 +45,26 @@ def loss_coteaching_plus(logits, logits2, labels, forget_rate, ind, noise_or_not
 
     logical_disagree_id=np.zeros(labels.size(), dtype=bool)
     disagree_id = []
-    for idx, p1 in enumerate(pred1): 
+    for idx, p1 in enumerate(pred1):
         if p1 != pred2[idx]:
-            disagree_id.append(idx) 
+            disagree_id.append(idx)
             logical_disagree_id[idx] = True
-    
+
     temp_disagree = ind*logical_disagree_id.astype(np.int64)
     ind_disagree = np.asarray([i for i in temp_disagree if i != 0]).transpose()
     try:
         assert ind_disagree.shape[0]==len(disagree_id)
     except:
         disagree_id = disagree_id[:ind_disagree.shape[0]]
-     
+
     _update_step = np.logical_or(logical_disagree_id, step < 5000).astype(np.float32)
     update_step = Variable(torch.from_numpy(_update_step)).cuda()
 
     if len(disagree_id) > 0:
         update_labels = labels[disagree_id]
-        update_outputs = outputs[disagree_id] 
-        update_outputs2 = outputs2[disagree_id] 
-        
+        update_outputs = outputs[disagree_id]
+        update_outputs2 = outputs2[disagree_id]
+
         loss_1, loss_2, pure_ratio_1, pure_ratio_2 = loss_coteaching(update_outputs, update_outputs2, update_labels, forget_rate, ind_disagree, noise_or_not)
     else:
         update_labels = labels
@@ -76,8 +76,8 @@ def loss_coteaching_plus(logits, logits2, labels, forget_rate, ind, noise_or_not
 
         loss_1 = torch.sum(update_step*cross_entropy_1)/labels.size()[0]
         loss_2 = torch.sum(update_step*cross_entropy_2)/labels.size()[0]
- 
+
         pure_ratio_1 = np.sum(noise_or_not[ind])/ind.shape[0]
         pure_ratio_2 = np.sum(noise_or_not[ind])/ind.shape[0]
-    return loss_1, loss_2, pure_ratio_1, pure_ratio_2  
+    return loss_1, loss_2, pure_ratio_1, pure_ratio_2
 
