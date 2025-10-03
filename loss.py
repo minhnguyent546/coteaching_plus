72686d0f975d181d1d21bf84e126b8d70a6b2526
minhnguyent546 <minhnguyent546@gmail.com>
2025-10-03 21:49:12 +0700

    fix: caught some bugs in `loss_coteaching_plus` function

--- loss.py
+++ loss.py
@@ -34,50 +34,48 @@ def loss_coteaching(logits1, logits2, labels, forget_rate, ind, noise_or_not):
 
     return torch.sum(loss_1_update), torch.sum(loss_2_update), pure_ratio_1, pure_ratio_2
 
-def loss_coteaching_plus(logits, logits2, labels, forget_rate, ind, noise_or_not, step):
-    outputs = F.softmax(logits, dim=1)
-    outputs2 = F.softmax(logits2, dim=1)
-
-    _, pred1 = torch.max(logits.data, 1)
-    _, pred2 = torch.max(logits2.data, 1)
+def loss_coteaching_plus(logits1, logits2, labels, forget_rate, ind, noise_or_not, step):
+    pred1 = torch.argmax(logits1, dim=1)
+    pred2 = torch.argmax(logits2, dim=1)
 
     pred1, pred2 = pred1.cpu().numpy(), pred2.cpu().numpy()
 
-    logical_disagree_id=np.zeros(labels.size(), dtype=bool)
-    disagree_id = []
+    logical_disagree_id = np.zeros(labels.size(), dtype=bool)
+    disagree_indices = []
     for idx, p1 in enumerate(pred1):
         if p1 != pred2[idx]:
-            disagree_id.append(idx)
+            disagree_indices.append(idx)
             logical_disagree_id[idx] = True
 
-    temp_disagree = ind*logical_disagree_id.astype(np.int64)
+    temp_disagree = ind * logical_disagree_id.astype(np.int64)
     ind_disagree = np.asarray([i for i in temp_disagree if i != 0]).transpose()
     try:
-        assert ind_disagree.shape[0]==len(disagree_id)
+        assert ind_disagree.shape[0] == len(disagree_indices)
     except:
-        disagree_id = disagree_id[:ind_disagree.shape[0]]
+        disagree_indices = disagree_indices[:ind_disagree.shape[0]]
 
     _update_step = np.logical_or(logical_disagree_id, step < 5000).astype(np.float32)
     update_step = Variable(torch.from_numpy(_update_step)).cuda()
 
-    if len(disagree_id) > 0:
-        update_labels = labels[disagree_id]
-        update_outputs = outputs[disagree_id]
-        update_outputs2 = outputs2[disagree_id]
+    if len(disagree_indices) > 0:
+        updated_labels = labels[disagree_indices]
+        updated_logits1 = logits1[disagree_indices]
+        updated_logits2 = logits2[disagree_indices]
 
-        loss_1, loss_2, pure_ratio_1, pure_ratio_2 = loss_coteaching(update_outputs, update_outputs2, update_labels, forget_rate, ind_disagree, noise_or_not)
+        loss_1, loss_2, pure_ratio_1, pure_ratio_2 = loss_coteaching(updated_logits1, updated_logits2, updated_labels, forget_rate, ind_disagree, noise_or_not)
     else:
-        update_labels = labels
-        update_outputs = outputs
-        update_outputs2 = outputs2
+        updated_labels = labels
+        updated_logits1 = logits1
+        updated_logits2 = logits2
 
-        cross_entropy_1 = F.cross_entropy(update_outputs, update_labels)
-        cross_entropy_2 = F.cross_entropy(update_outputs2, update_labels)
+        cross_entropy_1 = F.cross_entropy(updated_logits1, updated_labels)
+        cross_entropy_2 = F.cross_entropy(updated_logits2, updated_labels)
 
-        loss_1 = torch.sum(update_step*cross_entropy_1)/labels.size()[0]
-        loss_2 = torch.sum(update_step*cross_entropy_2)/labels.size()[0]
+        loss_1 = torch.sum(update_step * cross_entropy_1) / labels.shape[0]
+        loss_2 = torch.sum(update_step * cross_entropy_2) / labels.shape[0]
 
-        pure_ratio_1 = np.sum(noise_or_not[ind])/ind.shape[0]
-        pure_ratio_2 = np.sum(noise_or_not[ind])/ind.shape[0]
+        pure_ratio_1 = np.sum(noise_or_not[ind]) / ind.shape[0]
+        pure_ratio_2 = np.sum(noise_or_not[ind]) / ind.shape[0]
+
     return loss_1, loss_2, pure_ratio_1, pure_ratio_2
 
